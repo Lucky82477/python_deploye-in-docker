f98934e3d003f902f330cf3cb27237316fa1cc94
Lucky82477 <sreelakshmikadapaka@gmail.com>
2026-02-16 13:10:46 +0530

    Replace add route with hello route
    Removed the add route and replaced it with a hello route.

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/add', methods=['GET'])
-def add():
-    num1 = int(request.args.get('num1'))
-    num2 = int(request.args.get('num2'))
-    return jsonify({"result": num1 + num2})
+@app.route('/')
+def hello():
+    return "Hello from Dockerized Python on EC2!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
